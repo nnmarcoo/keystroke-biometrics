50c1d5a5f408c98519f92620891d79924a98a2e9
Marco <marcosteam7@gmail.com>
2024-10-17 15:02:45 -0400

    Update ops.rs

--- src/ops.rs
+++ src/ops.rs
@@ -23,15 +23,8 @@ pub fn create_user(user_name: &str) -> QueryResult<i32> {
 }
 
 pub fn remove_user(user_id: i32) -> QueryResult<usize> {
-    use crate::schema::metrics::dsl as metrics_dsl;
-    use crate::schema::pairs::dsl as pairs_dsl;
     use crate::schema::user::dsl as user_dsl;
-
     let mut conn = establish_connection().unwrap();
-
-    diesel::delete(pairs_dsl::pairs.filter(pairs_dsl::id.eq(user_id))).execute(&mut conn)?;
-
-    diesel::delete(metrics_dsl::metrics.filter(metrics_dsl::id.eq(user_id))).execute(&mut conn)?;
 
     diesel::delete(user_dsl::user.filter(user_dsl::id.eq(user_id))).execute(&mut conn)
 }
